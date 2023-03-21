916674f7930a5bd3b2ee6a92e4718edaf6caa0bb
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-03-21 18:11:44 +0700

    feat: add unit test main container component

---
+++ src/components/MainContainer.test.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import MainContainer from './MainContainer'
+
+const data = {
+  children: 'This is children',
+  className: 'test-class',
+}
+
+describe('MainContainer component', () => {
+  it('should handle children correctly', () => {
+    render(
+      <MainContainer data-testid='main-container'>
+        {data.children}
+      </MainContainer>
+    )
+    expect(screen.getByTestId('main-container')).toHaveTextContent(
+      data.children
+    )
+  })
+
+  it('should handle is center correctly', () => {
+    render(
+      <MainContainer isCenter data-testid='main-container'>
+        {data.children}
+      </MainContainer>
+    )
+    expect(screen.getByTestId('main-container')).toHaveClass('justify-center')
+  })
+
+  it('should handle className correctly', () => {
+    render(
+      <MainContainer className={data.className} data-testid='main-container'>
+        {data.children}
+      </MainContainer>
+    )
+    expect(screen.getByTestId('main-container')).toHaveClass(data.className)
+  })
+})
