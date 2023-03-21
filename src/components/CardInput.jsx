ae4bd2500f170b906b47f965b6add7343e64b982
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-03-21 15:31:55 +0700

    feat: add unit test card input component

--- src/components/CardInput.jsx
+++ src/components/CardInput.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-function CardInput({ children, title }) {
+function CardInput({ children, title, ...rest }) {
   return (
-    <div className='container'>
+    <div className='container' {...rest}>
       <div className='mx-auto max-w-2xl'>
         <div className='relative mt-5 flex flex-col items-center rounded-lg bg-white py-5 px-8 shadow-sm dark:bg-gray-700'>
           <div className='-mt-12 mb-4 rounded-md bg-white px-5 py-3 shadow-lg dark:bg-gray-600'>
---
+++ src/components/CardInput.test.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '@testing-library/jest-dom'
+import { render, screen } from '@testing-library/react'
+import CardInput from './CardInput'
+
+const data = {
+  title: 'This is title',
+  content: 'This is content',
+}
+
+describe('CardInput component', () => {
+  it('should handle children correctly', async () => {
+    render(
+      <CardInput data-testid='cardinput' title={data.title}>
+        {data.content}
+      </CardInput>
+    )
+    expect(screen.getByTestId('cardinput')).toHaveTextContent(data.title)
+    expect(screen.getByTestId('cardinput')).toHaveTextContent(data.content)
+  })
+})
