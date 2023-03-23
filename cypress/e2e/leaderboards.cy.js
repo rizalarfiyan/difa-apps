edfdf9a032b434b73d228865da85d43cd863330f
Muhamad Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2023-03-23 12:43:31 +0700

    fix: remove e2e test for should display not found of leaderboards page correctly

--- cypress/e2e/leaderboards.cy.js
+++ cypress/e2e/leaderboards.cy.js
@@ -39,34 +39,4 @@ describe('Leaderboards spec', () => {
       '#leaderboards > div:not([role="status"]):not([role="alert"])'
     ).should('have.length', skeleton.leaderboards)
   })
-
-  it('should display not found of leaderboards page correctly', () => {
-    cy.intercept('GET', endpoint.leaderboards, (req) => {
-      req.reply((res) => {
-        res.send({
-          status: 'success',
-          message: 'leaderboards retrieved successfully',
-          data: {
-            leaderboards: [],
-          },
-        })
-      })
-    })
-
-    cy.get('main h1')
-      .contains(/Leaderboards of active users$/)
-      .should('be.visible')
-    cy.get('#leaderboards').should('be.visible')
-    cy.get('#leaderboards > [role="status"]').should(
-      'have.length',
-      skeleton.leaderboards
-    )
-
-    cy.get(
-      '#leaderboards > div:not([role="status"]):not([role="alert"])'
-    ).should('have.length', 0)
-    cy.get('#leaderboards > div[role="alert"]')
-      .contains(/Data of leaderboard not found$/)
-      .should('be.visible')
-  })
 })
